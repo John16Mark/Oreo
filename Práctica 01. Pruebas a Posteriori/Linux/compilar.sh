e8e8fa0da7325b6779e0caece0d0b306f839d62b
John16Mark <92175973+John16Mark@users.noreply.github.com>
2023-03-08 10:42:42 -0600

    Documentación

--- Práctica 01. Pruebas a Posteriori/Linux/compilar.sh
+++ Práctica 01. Pruebas a Posteriori/Linux/compilar.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # compilar.sh
-# Version 1.0 Octubre 2022
-# Autor: Darío Quiñones
+# Version 1.0 marzo 2023
+# Autor: Juan Molina
+# Basado en el script de shell que implementó Darío Quiñones el semestre pasado en A y ED.
 
-# Un script de shell que compila todos los archivos .c en el directorio actual
+# Un script de shell que compila todos los archivos .c en el directorio src
 # y los guarda en el directorio bin
 
 # Variable de la carpeta de códigos fuente
@@ -16,10 +17,10 @@ TIM_DIR=src/tiempos/tiempo.c
 # Crear el directorio de binarios
 mkdir -p $BIN_DIR
 
-# Compilar todos los archivos .c en el directorio actual y guardarlos en BIN_DIR
-#gcc -o $BIN_DIR/burbujasim $SRC_DIR/BurbujaSimple.c $TIM_DIR
+# Compilar todos los archivos .c en el directorio src y guardarlos en BIN_DIR
 for f in $SRC_DIR/*.c
 do
+    # Variable del nombre del archivo pero regresando un directorio
     RET_DIR=${f##*/}
     echo "  Compilando $RET_DIR"
     gcc -o $BIN_DIR/${RET_DIR%.*} $f $TIM_DIR
