b6cd5fb117364488e78eb33e79f598ce020f7d73
dngxue <122043669+dngxue@users.noreply.github.com>
2023-03-13 20:48:01 -0600

    Add files via upload

---
+++ Práctica 01. Pruebas a Posteriori/Windows/Algoritmos/Mergesort.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void MergeSort(int A[], int p, int r);
+void Merge(int A[],int p, int q, int r);
+void imprimir(int *A, int n);
+
+int main(int argc, char *argv[])
+{
+	// Variables para la medición de tiempos
+	clock_t t_inicio, t_final;
+	double t_intervalo;
+	// Variables para el algoritmo
+	int i, j, n, *A;
+
+	// Recibir por argumento el tamaño de n y el valor a buscar
+	if (argc != 2)
+	{
+		printf("\n\n Para compilar el programa se necesita tamanio de arreglo");
+		printf("\n Ejemplo: %s 100", argv[0]);
+		exit(1);
+	}
+
+	// Tomar el argumento del main
+	n = atoi(argv[1]);
+	A = malloc(n * sizeof(int));
+	if (A == NULL)
+	{
+		printf("\nError al intentar reservar memoria para %d elementos\n", n);
+		exit(1);
+	}
+
+	// Leer de la entrada estándar los n valores y colocarlos en el arreglo de números
+	for (i = 0; i < n; i++)
+		scanf("%d", &A[i]);
+	printf("\n\n    MERGESORT\n   con %d n�meros",n);
+	// Inicia medición del tiempo
+	t_inicio = clock();
+	MergeSort(A,0,n-1);
+	t_final = clock();
+	t_intervalo = (double)(t_final - t_inicio) / (CLOCKS_PER_SEC);
+	printf("\n\nTiempo medido: %.8f segundos.\n", t_intervalo);
+	
+	imprimir(A, n);
+	
+	return 0;
+}
+
+void MergeSort(int A[], int p, int r)
+{
+	int q;
+
+	if (p<r)
+	{
+		q=(p+r)/2;
+		MergeSort(A,p,q);
+		MergeSort(A,q+1,r);
+		Merge(A,p,q,r);
+	}
+}
+
+
+void Merge(int A[], int p, int q, int r)
+{
+	int k, l=r-p+1, i=p, j=q+1;
+	//int C[l];
+	int *C = malloc(l * sizeof(int));
+	if (C == NULL)
+	{
+		printf("\nError al intentar reservar memoria para %d elementos\n", l);
+		exit(1);
+	}
+
+	for(k=0;k<l;k++)
+	{
+		if(i<=q&&j<=r)
+		{
+			if(A[i]<A[j])
+			{
+				C[k]=A[i];
+				i++;
+			}
+			else
+			{
+				C[k]=A[j];
+				j++;
+			}
+		}
+		else if(i<=q)
+		{
+			C[k]=A[i];
+			i++;
+		}
+		else
+		{
+			C[k]=A[j];
+			j++;
+		}
+		//printf("C[%d] = %d\n",k, C[k]);
+	}
+	for(k=p,i=0;k<=r;k++,i++)
+	{
+		A[k]=C[i];
+	}
+
+	free(C);
+}
+
+void imprimir(int *A, int n)
+{
+	int i, j;
+	printf("\n\n  ARREGLO ORDENADO:\n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("\n%d: %d",i,A[i]);
+	}
+}
