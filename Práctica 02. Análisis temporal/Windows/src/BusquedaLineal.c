67b5dfca04161ab2bc4ef66c63f7d98d72d505a8
John16Mark <92175973+John16Mark@users.noreply.github.com>
2023-04-25 21:14:33 -0600

    Cambios en diseño visual

--- Práctica 02. Análisis temporal/Windows/src/BusquedaLineal.c
+++ Práctica 02. Análisis temporal/Windows/src/BusquedaLineal.c
@@ -7,21 +7,29 @@ Implementación de la búsqueda lineal en C obtenida en https://www.geeksforgeeks.
 Toma n números enteros de la entrada estándar en la forma:
 > BusquedaLineal n k a0 a1 a2 a3 ... an (en linux)
 Imprime el tiempo que tomó la ejecución del algoritmo, e imprime la dirección de memoria donde se encontró el valor.
-*/
 
-//gcc BusquedaLineal.c -o BusquedaLineal tiempos/tiempo.c
-//BusquedaLineal 500000 82182077 < orden10millones.txt
+	COMPILAR:
+gcc BusquedaLineal.c -o BusquedaLineal.exe lib/TADColaDin.c
+
+	EJECUTAR:
+BusquedaLineal.exe 10000000 < numeros10millones.txt
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib/TADColaDin.h"
-#include <windows.h>
+#include <time.h>
 
 int Busqueda(int *A, int n, int k);
 void cargarArchivo(cola *c, char *direccion);
 
 int main(int argc, char *argv[])
 {
+	// Variables para la medición de tiempos
+	clock_t t_inicio, t_final;
+	double t_intervalo;
+	double t_acumulado = 0;
 	// Variable para determinar la longitud del arreglo
 	int n;
 	// Variable contadora para leer los números
@@ -43,29 +51,43 @@ int main(int argc, char *argv[])
 	// Lee el argumento
 	n = atoi(argv[1]);
 	A = malloc(n * sizeof(int));
-	// Lee de la entrada estándar los n valores y los coloca en el árbol
+	// Lee de la entrada estándar los n valores y los coloca en el arreglo
 	for (i = 0; i < n; i++)
 		scanf("%d", &A[i]);
 		
 	// Lee los números a buscar de la dirección "buscar.txt"
 	Initialize(&mi_cola);
 	cargarArchivo(&mi_cola, "buscar.txt");
+
+	printf("\n\n    B%cSQUEDA LINEAL\n    con %d n%cmeros", 233, n, 163);
 	
-	// Ejecución del algoritmo de búsqueda
+	// Ciclo para cada elemento de la cola
 	for(i = 1; i <= Size(&mi_cola); i++){
+
+		printf("\n\n Valor a encontrar: %d", Element(&mi_cola, i).n);
+		// Inicia la medición de tiempos
+		t_inicio = clock();
+		// Ejecución del algoritmo de búsqueda
 		p = Busqueda(A, n, Element(&mi_cola, i).n);
+		// Termina la medición de tiempos
+		t_final = clock();
+		t_intervalo = (double)(t_final - t_inicio) / (CLOCKS_PER_SEC);
+		t_acumulado += t_intervalo;
+		
 		// /*											Comentar si no se quiere imprimir la posición en donde se encontró
 		//Se imprime la posición del arreglo en la que se encontró o, en su defecto, -1 si no se encuentra en arreglo
-		printf("\n Valor a encontrar: %d", Element(&mi_cola, i).n);
 		if(p == -1){
-			printf("\n \033[91mNO SE ENCONTR%c EL N%cMERO\033[0m\n", 224, 233);
+			printf("\n \033[91mNO SE ENCONTR%c EL N%cMERO\033[0m", 224, 233);
 		}
 		else{
-			printf("\n \033[92mSe encontr%c en la posici%cn:\033[0m %d\n",162,162, p);
+			printf("\n \033[92mSe encontr%c en la posici%cn:\033[0m %d",162,162, p);
 		}
 		// */											Fin comentario
+		printf("\n Tiempo medido: %.8f segundos.\n", t_intervalo);
 	}
-	
+	t_acumulado /= Size(&mi_cola);
+	printf("\n\n    PROMEDIO DE MEDICI%cN DE TIEMPOS\n    %.8f segundos.\n", 224, t_acumulado);
+
 	//Libera la memoria del arreglo
 	free(A);
 	return 0;
