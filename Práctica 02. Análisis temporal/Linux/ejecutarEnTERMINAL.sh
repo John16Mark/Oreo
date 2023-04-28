198a5a77e02e513eec63270fdc1d31de71c720e0
John16Mark <92175973+John16Mark@users.noreply.github.com>
2023-04-28 11:33:17 -0600

    ejecutarEnTERMINAL.sh

---
+++ Práctica 02. Análisis temporal/Linux/ejecutarEnTERMINAL.sh
@@ -0,0 +1,44 @@
+
+# compilar.sh
+# Version 1.0 abril 2023
+# Autor: Juan Molina
+# Basado en el script de shell que implementó Darío Quiñones el semestre pasado en AyED.
+
+# Un script de shell que ejecuta todos los archivos .c en el directorio bin con un argumento
+# y los guarda la salida en un archivo de texto en la carpeta out
+
+# --------------------MODIFICAR ESTE VALOR-------------------------
+ALGORITMO=Exponencial
+n=10000000
+THREADS=4
+# -----------------------------------------------------------------
+# Variables
+BIN_DIR=bin
+SRC=numeros10millones.txt
+OUT_DIR=out
+
+# Comprobar que el directorio bin existe
+if [ ! -d $BIN_DIR ]; then
+    echo "El directorio $BIN_DIR no existe, utilice el script compilar.sh para compilar los algoritmos"
+    exit 1
+fi
+
+# Comprobar que el archivo de entrada existe
+if [ ! -f $SRC ]; then
+    echo "El archivo $SRC no esta en el directorio actual"
+    exit 1
+fi
+
+if [[ $f =~ ^(bin/Lineal) ]]; then
+    SRC=numeros10millones.txt
+else
+    SRC=10millones.txt
+fi
+#echo " SOURCE: $SRC"
+if [[ $f =~ (T)$ ]]; then
+    echo "  Ejecutando $RET_DIR $n con $THREADS hilos"
+    $BIN_DIR/$ALGORITMO $n $THREADS < $SRC
+else
+    echo "  Ejecutando $RET_DIR $n"
+    $BIN_DIR/$ALGORITMO $n < $SRC
+fi
